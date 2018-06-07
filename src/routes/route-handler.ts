4deecbad5a235e041a8e89ddb449d7b943168206
Rick <duulketariakan@yahoo.com>
2018-06-07 21:39:13 +0900

    Add basic Apollo and GraphQL server

--- src/routes/route-handler.ts
+++ src/routes/route-handler.ts
@@ -1,9 +1,48 @@
 import * as Express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+
 import client from '../db/config';
 
 const apiRouter = Express.Router();
 
-apiRouter.get('/', (_req, res) => {
+// Mock data
+const books = [
+    {
+        title: 'Harry Potter and the Sorcerer\'s stone',
+        author: 'J.K. Rowling',
+    },
+    {
+        title: 'Jurassic Park',
+        author: 'Michael Crichton',
+    },
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+    type Query { books: [Book] }
+    type Book { title: String, author: String }
+`;
+
+// The Resolvers
+const resolvers = {
+    Query: { books: () => books },
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+})
+
+// GraphQL endpoint
+apiRouter.use('/graphql', graphqlExpress({ schema }));
+
+// GraphiQL visual editor for queries
+apiRouter.get('/', graphiqlExpress({ endpointURL: '/graphql' }));
+
+// Test route
+apiRouter.get('/test', (_req, res) => {
     console.log({client});
     const date = new Date();
     res.send(`API route check: ${date}`);
